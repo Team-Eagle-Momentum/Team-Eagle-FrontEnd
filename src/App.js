1a86ab0c776f1456681b3215cc586c3b1aba8fb1
andres alcocer <devandres.tech@gmail.com>
2022-08-21 18:13:23 -0400

    refactored login.js component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import LoginForm from "./components/Login"
+import LoginForm from './components/Login/Login'
 import RegisterForm from './components/Register'
+
+const PrivateRoute = () => {}
 
 function App() {
   return (
@@ -11,11 +13,16 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
+        <Route path='/login' element={<LoginForm />} />
+        <Route path='/register' element={<RegisterForm />} />
+        {/* <Route path='/details/:id' element={<RegisterForm />} /> */}
       </Routes>
     </>
   )
 }
+
+// 1, check for auth token in state,
+// 2, if token is valid return route
+// 2, redirect to register form
 
 export default App
---
+++ src/components/Login/Login.js
@@ -0,0 +1,48 @@
+import { Link, Navigate } from 'react-router-dom'
+import { useState } from 'react'
+import './Login.css'
+
+export const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(username)
+    console.log(password)
+  }
+
+  return (
+    <>
+      <div className='login-wrap'>
+        <h2>Log In</h2>
+        <form onSubmit={handleSubmit}>
+          <div className='login-field'>
+            <label htmlFor='username-field'>username: </label>
+            <input
+              id='username-field'
+              onChange={(e) => setUsername(e.target.value)}
+              type='text'
+            />
+          </div>
+          <div className='login-field'>
+            <label htmlFor='password-field'>password: </label>
+            <input
+              id='password-field'
+              onChange={(e) => setPassword(e.target.value)}
+              type='password'
+            />
+          </div>
+          <div className='login-submit'>
+            <input type='submit' value='Log In' />
+            <Link className='login-new-user' to={'/register'}>
+              New user? Create an Account
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default LoginForm
