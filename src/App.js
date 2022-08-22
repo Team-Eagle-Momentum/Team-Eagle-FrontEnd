2835f91030ed59cc1c1694bbd212114c9500c61d
andres alcocer <devandres.tech@gmail.com>
2022-08-22 16:14:26 -0400

    updated App routes

--- src/App.js
+++ src/App.js
@@ -5,26 +5,15 @@ import Home from './components/Home'
 import LoginForm from './components/Login/Login'
 import RegisterForm from './components/Register'
 
-const PrivateRoute = () => {
-  const token = localStorage.get('auth_token')
-  if (token) {
-    return <Route />
-  } else {
-    Navigate('/')
-  }
-}
-
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/login' element={<LoginForm />} /> */}
-        {/* <Route path='/register' element={<RegisterForm />} /> */}
+        <Route path='/login' element={<LoginForm />} />
+        <Route path='/register' element={<RegisterForm />} />
         {/* <Route path='/details/:id' element={<RegisterForm />} /> */}
-        {/* <PrivateRoute element={<Details />} />
-        <PrivateRoute element={<SaveRoute />} /> */}
       </Routes>
     </>
   )
