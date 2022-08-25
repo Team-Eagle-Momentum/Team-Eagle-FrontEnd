63fbc755d503897eeca45ce477275caa9a631ea0
Metta Rolando <mnrolando3@gmail.com>
2022-08-24 22:59:09 -0400

    Worked on footer. Still not centering properly and struggling with aligning to bottom of page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,30 @@
-import { Link } from 'react-router-dom'
-import { Flex, ChakraProvider } from '@chakra-ui/react'
+import { Box, Flex, Divider, Link, Icon } from '@chakra-ui/react'
+import { } from '@chakra-ui/icons'
+import { GoMarkGithub } from 'react-icons/go'
 
-export default function Navbar() {
-    const token = localStorage.getItem('auth')
-
+export default function Footer() {
     return (
         <>
-            <ChakraProvider>
-                <Flex bg='brand.aqua' className='footer'>
-                </Flex>
-            </ChakraProvider>
+            <Divider
+                h='100px'>
+            </Divider>
+            <Flex
+                bg='brand.aqua'
+                className='footer'
+                alignItems='center'
+                gap='2'>
+                <Box>
+                    Made by Momentum Team 13 Eagles - August 2022
+                </Box>
+
+
+                <Link href='https://github.com/Team-Eagle-Momentum' isExternal>
+                    <Icon
+                        as={GoMarkGithub}
+                        mt='5px'>
+                    </Icon>
+                </Link>
+            </Flex>
         </>
     )
 }
