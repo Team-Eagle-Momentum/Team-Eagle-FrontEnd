3c42503c23480382e3898129392580b310f24bd4
andres alcocer <devandres.tech@gmail.com>
2022-08-30 15:20:12 -0400

    created login navbar mobile styles

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 export const splitAddress = (addressText) => {
   const addressArray = addressText.split(',')
   let city = ''
@@ -13,4 +15,42 @@ export const splitAddress = (addressText) => {
 
 export const roundNumber = (numberVal) => {
   return (Math.round(numberVal * 100) / 100).toFixed(1)
+}
+
+const height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight
+const width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth
+
+export const useViewport = () => {
+  const [windowDimensions, setWindowDimensions] = useState({ height, width })
+
+  const deriveWindowDimensions = () => {
+    const height =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight
+
+    const width =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth
+
+    setWindowDimensions({ height, width })
+  }
+
+  useEffect(() => {
+    deriveWindowDimensions()
+    window.addEventListener('resize', deriveWindowDimensions)
+
+    return () => {
+      window.removeEventListener('resize', deriveWindowDimensions)
+    }
+  }, [])
+
+  return [windowDimensions]
 }
