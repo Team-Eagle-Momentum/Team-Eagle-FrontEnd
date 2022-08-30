c21cd032b8daf47a94a1881b53e3bc38c54588bc
Metta Rolando <mnrolando3@gmail.com>
2022-08-30 09:54:26 -0400

    Updated link styles and spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,10 +23,7 @@ export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <Flex
-      bg={useColorModeValue('brand.aqua', 'dark.darkest')}
-      className='nav-container'
-    >
+    <Flex bg={useColorModeValue('brand.aqua', 'dark.darkest')}>
       <Square>
         <Image
           m='5px'
@@ -36,53 +33,54 @@ export default function Navbar() {
           alt='CommutilatorLogo'
         />
       </Square>
-      <Box flexDirection='column' alignItems='left' w='50vw' m='5px'>
-        <Box className='title'>
-          <Link to={'/'}>Commutilator</Link>
-        </Box>
-        <Box className='subtitle'>A Commute Calculator for Today's Driver</Box>
-      </Box>
-      <Spacer />
-      <ul className='nav-items'>
-        <li
+      <Box alignItems='left' w='50vw' m='5px'>
+        <Box className='title'
           onClick={() => {
             setCurrentStep(1)
-          }}
-          className='nav-item'
-        >
+          }}>
+          <Link to={'/'}>Commutilator</Link>
+        </Box>
+        <Box className='subtitle'>
+          A Commute Calculator for Today's Driver
+        </Box>
+      </Box>
+      <Spacer />
+      <Flex justify='center' wrap='wrap' w='30vw' className='subtitle'>
+        <Button m='10px' variant='link' color='black' fontWeight='500'
+          onClick={() => {
+            setCurrentStep(1)
+          }}>
           <Link to={'/'}>New Calculation</Link>
-        </li>
+        </Button>
         {!token ? (
           <>
-            <li className='nav-item'>
+            <Button m='10px' variant='link' color='black' fontWeight='500'>
               <Link to={'/login'}>Login</Link>
-            </li>
-            <li className='nav-item'>
+            </Button>
+            <Button m='10px' variant='link' color='black' fontWeight='500'>
               <Link to={'/register'}>Register</Link>
-            </li>
+            </Button>
           </>
         ) : (
           <>
-            <li className='nav-item'>
+            <Button m='10px' variant='link' color='black' fontWeight='500'>
               <Link to={'/results'}>My Results</Link>
-            </li>
-            <li className='nav-item'>
-              <span
-                onClick={() => {
-                  localStorage.clear()
-                  setResultCalculation({
-                    result: { weekly: '' },
-                  })
-                  setCurrentStep(1)
-                  navigate('/')
-                }}
-              >
-                Logout
-              </span>
-            </li>
+            </Button>
+            <Button m='10px' variant='link' color='black' fontWeight='500'
+              onClick={() => {
+                localStorage.clear()
+                setResultCalculation({
+                  result: { weekly: '' },
+                })
+                setCurrentStep(1)
+                navigate('/')
+              }}
+            >
+              Logout
+            </Button>
           </>
         )}
-      </ul>
+      </Flex>
       <Center>
         <Button className='switch-item' onClick={toggleColorMode}>
           {colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
