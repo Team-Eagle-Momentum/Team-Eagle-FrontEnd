b2994c3348980aeeda1fcbda55fb934c5770c732
Metta Rolando <mnrolando3@gmail.com>
2022-08-30 14:57:40 -0400

    Updated result list styling to make variant dependent on color mode for better legibility, use gray from branding, and clean up code. Updated spacing on theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,5 @@
-// theme.js
+import { extendTheme, useColorModeValue } from '@chakra-ui/react'
 
-// 1. import `extendTheme` function
-import { extendTheme } from '@chakra-ui/react'
-
-// 2. Add your color mode config
 const config = {
   initialColorMode: 'light',
   useSystemColorMode: false,
@@ -27,9 +23,6 @@ const colors = {
   },
 }
 
-
-
-// 3. extend the theme
 const theme = extendTheme({ config, colors })
 
 export default theme
